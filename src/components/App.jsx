8f55000a12bbc98c7c66aceb94d4de23d8ae011f
Michal Raszczyk <mraszczyk@gmail.com>
2023-02-10 17:54:40 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
 import style from "./mainstyle.module.css";
 
-// import user from "./Profile/user.json";
-// import data from "./Statistics/data.json";
-// import friends from "./FriendsList/friends.json";
-// import transactionData from "./TransactionHistory/transactions.json";
+import user from "./Profile/user.json";
+import data from "./Statistics/data.json";
+import friends from "./FriendsList/friends.json";
+import transactionData from "./TransactionHistory/transactions.json";
 
-// import { Profile } from "./Profile/Profile.jsx";
-// import { Statistics } from "./Statistics/Statistics.jsx";
-// import { FriendsList } from "./FriendsList/FriendsList.jsx";
-// import { TransactionsHistory } from "./TransactionHistory/TransactionsHistory.jsx";
+import { Profile } from "./Profile/Profile.jsx";
+import { Statistics } from "./Statistics/Statistics.jsx";
+import { FriendsList } from "./FriendsList/FriendsList.jsx";
+import { TransactionsHistory } from "./TransactionHistory/TransactionsHistory.jsx";
 import { Counter } from "./Counter/Counter";
 
 export const App = () => {
   return (
     <div className={style.container}>
-      {/* <div className={style.box}>
+      <div className={style.box}>
         <>
           <Profile
             username={user.username}
@@ -33,7 +33,10 @@ export const App = () => {
       </div>
       <div className={style.box}>
         <TransactionsHistory data={transactionData} />
-      </div> */}
+      </div>
+
+
+      
       <div className={style.box}>
         <Counter />
       </div>
