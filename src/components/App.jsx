0e504da62966501adbdaaf96a1b35be12e357023
Michal Raszczyk <mraszczyk@gmail.com>
2023-02-10 17:42:44 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import style from "./mainstyle.module.css";
 
-import user from "./Profile/user.json";
-import data from "./Statistics/data.json";
-import friends from "./FriendsList/friends.json";
-import transactionData from "./TransactionHistory/transactions.json";
+// import user from "./Profile/user.json";
+// import data from "./Statistics/data.json";
+// import friends from "./FriendsList/friends.json";
+// import transactionData from "./TransactionHistory/transactions.json";
 
-import { Profile } from "./Profile/Profile.jsx";
-import { Statistics } from "./Statistics/Statistics.jsx";
-import { FriendsList } from "./FriendsList/FriendsList.jsx";
-import { TransactionsHistory } from "./TransactionHistory/TransactionsHistory.jsx";
+// import { Profile } from "./Profile/Profile.jsx";
+// import { Statistics } from "./Statistics/Statistics.jsx";
+// import { FriendsList } from "./FriendsList/FriendsList.jsx";
+// import { TransactionsHistory } from "./TransactionHistory/TransactionsHistory.jsx";
 import { Counter } from "./Counter/Counter";
 
 export const App = () => {
